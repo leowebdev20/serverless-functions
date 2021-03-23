50d0db3aea35a77743c1f507c217fa50726698ec
leowebdev20 <serlebeiso@gmail.com>
2021-03-23 22:17:31 +0100

    third commit

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -7,8 +7,16 @@ const fetchData = async () => {
         const {data} = await axios.get('/api/2-basic-api');
         result.textContent = data;
         const products = data.map((product) => {
-            console.log(product);
-        })
+            const {image:{url},name,price} = product
+            return `<article class="product">
+            <img src="${url}" alt="${name}" />
+            <div class="info">
+              <h5>${name}</h5>
+              <h5 class="price">$${price}</h5>
+            </div>
+          </article>`
+        }).join('');
+        result.innerHTML = products;
     } catch (error) {
         console.log(error.response);   
         result.textContent = error.response.data 
