a761f34f677acbd76e1f4f2e200a050660340b63
leowebdev20 <serlebeiso@gmail.com>
2021-04-01 22:24:02 +0200

    product id

--- examples/3-airtable/product.js
+++ examples/3-airtable/product.js
@@ -1,7 +1,14 @@
 const result = document.querySelector('.result');
 
 const fetchProduct = async () => {
-
+    result.innerHTML = `<h2>Loading...</h2>`;
+    try {
+        const id = window.location.search;
+        const data = await axios.get(`/api/3-product${id}`)
+        const {name, desc, price, image} = fields;
+    } catch (error) {
+        result.innerHTML = `<h2>${error.response.data}</h2>`;
+    }
 };
 
 fetchProduct();
