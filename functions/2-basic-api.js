0469cba24928336b63048da6396448137f67bffc
leowebdev20 <serlebeiso@gmail.com>
2021-04-04 11:19:43 +0200

    fix cors

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data')
 
 exports.handler = async (event, context) => {
     return {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
         statusCode: 200,
         body: JSON.stringify(items)
     }
